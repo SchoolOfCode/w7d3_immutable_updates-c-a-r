8f6e8139e941510e7fd5952a529afb209cb6f9c9
Alex Mulgrew <alexpmulgrew@gmail.com>
2022-01-06 12:22:02 +0000

    objects

--- index.js
+++ index.js
@@ -2,32 +2,32 @@
 
 // Immutably add an item to the end of an array
 export function addToEnd(array, item) {
-   const newArray = [...array, item];
-   return newArray;
+  const newArray = [...array, item];
+  return newArray;
 }
 
 // Immutably add an item to the beginning of an array
 export function addToBeginning(array, item) {
-   const newArray = [item, ...array];
-   return newArray;
+  const newArray = [item, ...array];
+  return newArray;
 }
 
 // Immutably insert an item at a specific position/index within an array
 export function insertItem(array, item, index) {
-   const newArray = [...array.slice(0, index), item, ...array.slice(index)];
-   return newArray;
+  const newArray = [...array.slice(0, index), item, ...array.slice(index)];
+  return newArray;
 }
 
 // Immutably replace an item at a specific position/index within an array
 export function replaceItem(array, item, index) {
-   const newArray = [...array.slice(0, index), item, ...array.slice(index + 1)];
-   return newArray;
+  const newArray = [...array.slice(0, index), item, ...array.slice(index + 1)];
+  return newArray;
 }
 
 // Immutably remove an item at a specific position/index within an array
 export function removeItem(array, index) {
-   const newArray = [...array.slice(0, index), ...array.slice(index + 1)];
-   return newArray;
+  const newArray = [...array.slice(0, index), ...array.slice(index + 1)];
+  return newArray;
 }
 
 //Objects:
@@ -37,7 +37,10 @@ export function removeItem(array, index) {
 //     updateName({ name: "Abe" }, "Barbara")
 // should give back:
 //     { name: "Barbara"}
-export function updateName(object, newName) {}
+export function updateName(object, newName) {
+  const newObject = { ...object, name: newName };
+  return newObject;
+}
 
 // Immutably update the object so that the value under the "needsACupOfTea" property becomes the opposite of what it was.
 // Any other properties in the object should be maintained.
@@ -45,7 +48,9 @@ export function updateName(object, newName) {}
 //     toggleTeaStatus({ name: "Abe", needsACupOfTea: false })
 // should give back:
 //     { name: "Abe", needsACupOfTea: true }
-export function toggleTeaStatus(object) {}
+export function toggleTeaStatus(object) {
+  const newObject = { ...object, needsACupOfTea: true };
+}
 
 // Combo Time!!
 
